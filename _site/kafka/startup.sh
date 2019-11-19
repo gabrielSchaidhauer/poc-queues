4eebc9f0cc545f5768ecd5941ac36115ad08024c
Gabriel Schaidhauer <gabriel@Gabriels-MacBook-Pro.local>
2019-11-19 07:53:52 -0300

    Removing weird files

--- _site/kafka/startup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-(nohup kafka_2.12-2.3.0/bin/zookeeper-server-start.sh kafka_2.12-2.3.0/config/zookeeper.properties>zookeeper.log &) &&
-(nohup kafka_2.12-2.3.0/bin/kafka-server-start.sh kafka_2.12-2.3.0/config/server.properties>kafka.log &) &&
-tail -f kafka.log
-
-
-
